741069cb7f5ab5444d3e367364e0103279af2e19
Matthew Stewart <mstewart77@qub.ac.uk>
2025-01-28 17:14:14 +0000

    updating isAnagram challenge

--- Arrays/isAnagram.py
+++ Arrays/isAnagram.py
@@ -1,18 +1,37 @@
 class Solution:
 
-    def sortedString(self, string: str):
-        return ''.join(sorted(string))
+# Sorting into alphabetical order has a time complexity of O(n log n) - where n
+# is the length of the string, frequency count approach belows provides a more
+# efficient O(n) solution
+
+#     def sortedString(self, string: str):
+#         return ''.join(sorted(string))
+
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         s_sort = self.sortedString(s)
+#         t_sort = self.sortedString(t)
+
+#         return s_sort == t_sort
+
+# solution = Solution()
+# s = "racecar"
+# t = "carrace"
+# print(solution.isAnagram(s,t))
 
     def isAnagram(self, s: str, t: str) -> bool:
-        s_sort = self.sortedString(s)
-        print(s_sort)
-        t_sort = self.sortedString(t)
-        print(t_sort)
+        if len(s) != len(t):
+            return False
 
-        return s_sort == t_sort
+        freq = {}
+        for char in s:
+            freq[char] = freq.get(char, 0) + 1
+
+        for char in t:
+            if char not in freq or freq[char] == 0:
+                return False
+            freq[char] -= 1
+
+        return all(value == 0 for value in freq.values())
 
 solution = Solution()
-s = "racecar"
-t = "carrace"
-print(solution.isAnagram(s,t))
-
+print(solution.isAnagram("ratt", "carr")) # Expected False
