b966570b02f3dcfe9b64d8cd26b8b83c23bb39e4
Matthew <100765186+MatthewStewart18@users.noreply.github.com>
2025-06-08 00:27:57 +0100

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/version/python/42-TrappingRainWater_20250608_002757.py
@@ -0,0 +1,22 @@
+# Last updated: 08/06/2025, 00:27:57
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height: return 0
+
+        l,r = 0,len(height) - 1
+
+        maxl = height[l]
+        maxr = height[r]
+        res = 0
+
+        while (l < r):
+            if maxl <= maxr:
+                l += 1
+                maxl = max(maxl, height[l])
+                res += maxl - height[l]
+            else:
+                r -= 1
+                maxr = max(maxr, height[r])
+                res += maxr - height[r]
+        return res
+            
