4e1bb5e7f79b497d9d9589271ca7192ffa3b8163
Matthew <100765186+MatthewStewart18@users.noreply.github.com>
2025-06-08 02:07:03 +0100

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/version/python/20-ValidParentheses_20250608_020703.py
@@ -0,0 +1,16 @@
+# Last updated: 08/06/2025, 02:07:03
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        mapping = {')': '(', ']': '[', '}': '{'}
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping:
+                if not stack or stack[-1] != mapping[char]:
+                    return False
+                stack.pop()
+            else:
+                continue
+        return not stack
+        
